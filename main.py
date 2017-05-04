ed0fd564d7e7e0260e43d32c81d9675da3f32251
Sebastian Schmittner <sebastian@schmittner.pw>
2017-05-05 00:55:30 +0200

    first working version

--- main.py
+++ main.py
@@ -25,69 +25,108 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 
+import json
+import logging
+import os
 import requests
 import re
-from getpass import getpass
-import logging
-import json
 import sys
 
 from collections import deque
 
-url_follow_queue = deque()
-"pool of urls to visit next"
 
-visited_urls = set()
-"do not visit those again"
+class Grabber(object):
 
-session = requests.Session()
+    def __init__(self):
+            self.url_follow_queue = deque()
+            "pool of urls to visit next"
 
-config = {}
-"Holds the configuration, typically loaded from a json file."
+            self.visited_urls = set()
+            "do not visit those again"
 
+            self.session = requests.Session()
 
-def filename_from_url(url):
-    match = re.search("[^/]+$", url)
-    if match:
-        return match.group()
-    return None
+            self.config = {}
+            "Holds the configuration, typically loaded from a json file."
 
+    def filename_from_url(self, url):
+        match = re.search("[^/]+$", url)
+        if match:
+            return match.group()
+        return None
 
-def process_found_url(url):
-    if url in visited_urls:
-        return
-    visited_urls.add(url)
+    def process_found_url(self, url):
+        if url in self.visited_urls:
+            return
+        self.visited_urls.add(url)
 
-    for reg in config["follow"]:
-        if re.match(reg, url):
-            url_follow_queue.append(url)
+        logging.debug("processing %s", url)
 
-    for reg in config["download"]:
-        if re.match(reg, url):
-            result = session.get(url)
-            filename = filename_from_url(result.url)
-            if not filename:
-                logging.error("Can not determine filename from %s", result.url)
-                return
-            with open(config["target"] + filename, 'w') as out_file:
-                out_file.write(result.content)
+        for reg in self.config["follow"]:
+            if re.match(reg, url):
+                self.url_follow_queue.append(url)
 
+        for reg in self.config["download"]:
+            if re.match(reg, url):
+                logging.info("Downloading %s", url)
+                result = self.session.get(url)
+                if not result.ok:
+                    logging.error("Error fetching file.")
+                    continue
+                filename = self.filename_from_url(result.url)
+                if not filename:
+                    logging.error("Can not determine filename from %s",
+                                  result.url)
+                    return
+                if len(filename) > 64:
+                    filename = filename[-64:]
+                with open(self.config["target"] + "/" + filename,
+                          'wb') as out_file:
+                    out_file.write(result.content)
 
-def visit_next_url():
-    url = url_follow_queue.popleft()
-    r = session.get(url)
+    def visit_next_url(self):
+        url = self.url_follow_queue.popleft()
+        r = self.session.get(url)
 
-    # find more urls
-    for m in re.finditer(r"""(https?://[^\s<>]+)|href=['"]([^"']+)""", r.text):
-        for g in m.groups():
-            if g:
-                process_found_url(g)
+        # find more urls
+        for m in re.finditer(r"""(https?://[^\s<>]+)|href=['"]([^"']+)""",
+                             r.text):
+            for g in m.groups():
+                if g:
+                    logging.debug("raw link %s", g)
+                    if re.match("https?://.*", g):
+                        new_url = g
+                    elif g[:2] == "//":
+                        new_url = "http:" + g
+                    else:
+                        new_url = url
+                        remove_trail_match = re.match("(.*)/[^/]*$", url)
+                        if remove_trail_match:
+                            new_url = remove_trail_match.group(1)
+                            
+                        if new_url[-1:] == "/":
+                            new_url = new_url[:-1]
+                        if not g[:1] == "/":
+                            new_url += "/"
+                        new_url += g
+                    logging.debug("corrected link %s", new_url)
+                    self.process_found_url(new_url)
 
 
 def main():
     """The main function gets a list of all mailing lists on the given
     server and performs (an) action(s) on all lists.
 
+    url_follow_queue = deque()
+    "pool of urls to visit next"
+
+    visited_urls = set()
+    "do not visit those again"
+
+    session = requests.Session()
+
+    config = {}
+    "Holds the configuration, typically loaded from a json file."
     """
     import argparse
 
@@ -147,30 +186,44 @@ def main():
 
     print("Log messages above level: {}".format(logger_cfg["level"]))
 
+    grabber = Grabber()
+
     if args.config:
         with open(args.config, "r") as cfg_file:
-            config = json.load(cfg_file)
+            grabber.config = json.load(cfg_file)
 
     if args.url:
-        config["url"] = args.url
+        grabber.config["url"] = args.url
 
-    if not config.get("url", None):
+    if not grabber.config.get("url", None):
         logging.critical("No base url(s) given.")
         parser.print_help()
         sys.exit(1)
     else:
-        logging.info("url: '%s'", config["url"])
+        logging.info("url: '%s'", grabber.config["url"])
 
-    if not config.get("download", None):
-        config["download"] = args.download
+    if not grabber.config.get("download", None):
+        grabber.config["download"] = args.download
 
-    if not config.get("follow", None):
-        config["follow"] = args.follow
+    if not grabber.config.get("follow", None):
+        grabber.config["follow"] = args.follow
 
-    url_follow_queue = deque(config["url"])
-    logging.info("Starting link tree traversal at %s", url_follow_queue)
-    while url_follow_queue:
-        visit_next_url()
+    if not grabber.config.get("target", None):
+        grabber.config["target"] = args.target
+
+    if grabber.config["target"][-1:] == "/":
+        grabber.config["target"] = grabber.config["target"][:-1]
+
+    grabber.config["target"] = os.path.abspath(grabber.config["target"])
+    if not os.path.isdir(grabber.config["target"]):
+        os.mkdir(grabber.config["target"])
+
+    grabber.url_follow_queue = deque(grabber.config["url"])
+    logging.info("Starting link tree traversal at %s",
+                 grabber.url_follow_queue)
+    # main loop
+    while grabber.url_follow_queue:
+        grabber.visit_next_url()
 
 
 # goto main
